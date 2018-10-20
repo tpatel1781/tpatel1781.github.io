263a8b5a9c30d2a2a59767a976f4ebf166f5820f
Spencer Berenson <spencer@berenson.me>
2018-10-20 14:38:01 -0400

    Create PostCSS config

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	plugins: [
+		require("cssnano")({
+			preset: [
+				"default",
+				{
+					discardComments: {
+						removeAll: true
+					}
+				}
+			]
+		}),
+		require("autoprefixer")({
+			grid: true
+		})
+	]
+};
